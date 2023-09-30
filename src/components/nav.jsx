a34dbbf03998dbcf24bd767d81ccbd555c28b6cd
JustinBenito <justinbenito1974@gmail.com>
2023-09-30 21:17:10 +0530

    values

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -15,14 +15,14 @@ const Nav = React.forwardRef((props, ref) => {
 <nav className="bg-white border-b-4 border-[#005197] dark:bg-gray-900">
   <div className="max-w-screen-xl gap-4 flex flex-row  items-center justify-center mx-auto p-4">
     <a href="https://www.ssn.edu.in/" class="flex items-center">
-        <img src="https://www.ssn.edu.in/wp-content/uploads/2019/12/logo.jpg" className=" w-17 " alt="SSN Logo" />   
+        <img src="https://www.ssn.edu.in/wp-content/uploads/2019/12/logo.jpg" className="md:w-32 md:h-20 w-24" alt="SSN Logo" />   
     </a>
     <div className='flex-col flex text-center'>
     <h1 className='md:text-5xl font-bold text-xl text-[#005197]'><span className='italic'>SSN</span>  College of Engineering</h1>
     <h1 className='md:text-xl font-bold text-sm'>Department of Electronics and Communication</h1>
     </div>
     <a href="https://signalprocessingsociety.org/community-involvement/seasonal-schools" class="flex items-center">
-      <img src="https://signalprocessingsociety.org/sites/default/files/uploads/images/logos/sps_75anniv_logos/CMYK/SPS_75Years_Color_CMYK.jpg" class="md:h-24 h-14" alt="IEEE SPS Logo" />   
+      <img src="https://signalprocessingsociety.org/sites/default/files/uploads/images/logos/sps_75anniv_logos/CMYK/SPS_75Years_Color_CMYK.jpg" class="md:h-24 md:w-28 w-14" alt="IEEE SPS Logo" />   
     </a>
   </div>
 </nav>
