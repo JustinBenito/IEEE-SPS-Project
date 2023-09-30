3c1e35d7694e6616a9516a1969969ac7f9a5b7ec
JustinBenito <justinbenito1974@gmail.com>
2023-09-30 17:06:51 +0530

    very small edit

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -7,14 +7,14 @@ const Nav = () => {
 <nav class="bg-white border-b-4 border-[#005197] dark:bg-gray-900">
   <div class="max-w-screen-xl gap-4 flex flex-row  items-center justify-center mx-auto p-4">
     <a href="https://ssn.edu.in/" class="flex items-center">
-        <img src="https://www.ssn.edu.in/wp-content/uploads/2019/12/logo.jpg" class="w-18" alt="Flowbite Logo" />   
+        <img src="https://www.ssn.edu.in/wp-content/uploads/2019/12/logo.jpg" class="w-18" alt="SSN Logo" />   
     </a>
     <div className='flex-col flex text-center'>
     <h1 className='md:text-5xl font-bold text-xl text-[#005197]'><span className='italic'>SSN</span>  College of Engineering</h1>
     <h1 className='md:text-xl font-bold text-sm'>Department of Electronics and Communication</h1>
     </div>
     <a href="https://signalprocessingsociety.org/community-involvement/seasonal-schools" class="flex items-center">
-        <img src="https://signalprocessingsociety.org/sites/default/files/uploads/images/logos/sps_75anniv_logos/CMYK/SPS_75Years_Color_CMYK.jpg" class="md:h-24 h-24" alt="Flowbite Logo" />   
+        <img src="https://signalprocessingsociety.org/sites/default/files/uploads/images/logos/sps_75anniv_logos/CMYK/SPS_75Years_Color_CMYK.jpg" class="md:h-24 h-14" alt="IEEE SPS Logo" />   
     </a>
   </div>
 </nav>
