6ab3ec1d00c8bedd0da24180f8ae4fd600b1ef23
JustinBenito <justinbenito1974@gmail.com>
2024-02-04 23:20:44 +0530

    routes

--- src/components/ssn.jsx
+++ src/components/ssn.jsx
@@ -6,21 +6,35 @@ const SSN = () => {
     <h1 className='text-3xl text-center p-4 font-bold mt-5 border-b-2' >Routes</h1>
 
     <div className='mr-4 ml-4'>
-    <p className='text-md text-justify p-4 mt-5 bg-slate-300 rounded-lg mb-5'><strong className='text-xl'>Route 1:</strong> <br></br>
-For Participants who arrive by Train to Egmore Station For those who are coming from South Tamil Nadu, it is informed that all the trains going to Chennai Egmore station stop at Tambaram. Tambaram bus stand is adjacent to Tambaram Railway station.
-Those who are coming from south Tamil Nadu by bus also can get down at Perungulathur and then they can cross the road to reach the bus stop and take the following buses: Route no. from Tambaram to SSN College of Engineering: Route no.: 515 & 115 - Mamallapuram Route no. : 555, 55K & 566 – Thirupporur These buses will reach SSN college of Engineering via Tambaram and Perungulathur. It takes about 45-55 minutes to reach SSN College.
+    <p className='text-md text-justify p-4 mt-5 bg-slate-300 rounded-lg mb-5'><strong className='text-xl'>Route 1 (For Participants who arrive by Train to Tambaram Station):
+</strong> <br></br>
+For those who are coming from South Tamil Nadu, it is informed that all the trains going to Chennai Egmore station stop at Tambaram. Tambaram bus stand is adjacent to Tambaram Railway station. Those who are coming from south Tamil Nadu by bus also can get down at Perungulathur and then they can cross the road to reach the bus stop and take the following buses: From Tambaram to Thiruporur/Mamallapuram, Route no.: 515, 555, 55K, 566 & 115. These buses will reach SSN college of Engineering via Tambaram and Perungulathur. It takes about 45-55 minutes to reach College bus stop.
+
 </p>
 
-<p className='text-md text-justify p-4 mt-5 bg-slate-100 rounded-lg mb-5'><strong className='text-xl'>Route 2 (For Participants who arrive by Train to Chennai Central Station):</strong> <br></br>
-OPTION (Broadway to SSN College of Engineering)
-Those who are coming from North can use this route. At the gate of Chennai Central Railway station, you can board a bus to Broadway. Normally all the buses entering the central station and stopping at the bay will go to Broadway bus stand. It takes 4-6 minutes to reach Broadway. At Broadway bus stand you can take the following route buses to Kelambakkam (which is about 4 kms. from the college). Route no. : 102 and 102X AC - Kelambakkam Route no. : 521- Thirupporur It takes 1hr 30 minutes to reach Kelambakkam from Broadway. From Kelambakkam the participant can board any bus going towards Thirupporur / Mamallapuram and get down at SSN college of Engineering
+<p className='text-md text-justify p-4 mt-5 bg-slate-100 rounded-lg mb-5'><strong className='text-xl'>Route 2 (For Participants who arrive by Train to Chennai Central Station): 
+</strong> <br></br>
+At the gate of Chennai Central Railway station, you can board a bus to Broadway. Normally all the buses entering the central station and stopping at the bay will go to Broadway bus stand. It takes 4-6 minutes to reach Broadway. At Broadway bus stand you can take the following route buses to Kelambakkam (which is about 4 kms. from the college). Route no.: 102 (Kelambakkam), Route no.: 109 (Kovalam. You can get down at Kelambakkam), Route no.: 102X (Thiruporur). It takes around 1hr 30 minutes to reach Kelambakkam from Broadway. From Kelambakkam the participant can board any bus going towards Thiruporur/Mamallapuram and get down at SSN college of Engineering or take an auto from Kelambakkam.
+
 </p>
 
-<p className='text-md text-justify p-4 mt-5 bg-slate-300 rounded-lg mb-5'><strong className='text-xl'>Route 3 (For Participants who arrive by bus) Koyambedu to SSN College of Engineering :</strong> <br></br>
-Those who are coming from north Tamil Nadu by bus can use this route. At Koyambedu bus stand there are two systems - One is for moffusel bus and the other one for local bus. From Koyembedu Moffusel bus Stand you can board the bus (Route no 119) to Kalppakkam/Mamallapuram, which goes via SSN College of Engineering. For boarding local bus, that is just outside of the Koyembedu bus stand you can board the bus (Route no 570) to Kelambakkam. Both the buses cover the distance about 1.30 hrs to 2 hrs depending on the traffic. </p>
+<p className='text-md text-justify p-4 mt-5 bg-slate-300 rounded-lg mb-5'><strong className='text-xl'>Route 3 (For Participants who arrive by bus to Koyambedu)
+</strong> <br></br>
+Those who are coming from north Tamil Nadu by bus can use this route. At Koyambedu bus stand there are two systems - One is for moffusil bus and the other one for local bus. From Koyembedu Moffusil bus stand you can board the bus (Route no. 119) to Kalppakkam/Mamallapuram, which goes via SSN College of Engineering. For boarding local bus, that is just outside of the Koyembedu bus stand you can board the bus (Route no. 570) to Kelambakkam. Both the buses cover the distance about 1.30 hrs to 2 hrs depending on the traffic.
+ </p>
 
-<p className='text-md text-justify p-4 mt-5 bg-slate-100 rounded-lg mb-5'><strong className='text-xl'>Route 4 (For Participants who arrive by flight to Chennai Airport)</strong> <br></br>
-Participants coming in flight can take bus airport to Tambaram /Guindy to Kelambakkam/Tiruporur to reach SSN. <br /> Can use Cab (Mr Muthu- Toyoto Etios -9042107358)
+<p className='text-md text-justify p-4 mt-5 bg-slate-100 rounded-lg mb-5'><strong className='text-xl'>Route 4 (For Participants who arrive from Pondicherry)
+</strong> <br></br>
+Those who are coming from Pondicherry, can get down at Poonjeri. From there, they can take MTC bus route no. 515 which goes to Tambaram. They can get down at SSN College of Engineering. It would take around 30 to 40 minutes to reach College.
+
+</p>
+
+<p className='text-md text-justify p-4 mt-5 bg-slate-300 rounded-lg mb-5'><strong className='text-xl'>Route 5 (For Participants who arrive by flight to Chennai Airport) 
+
+</strong> <br></br>
+Participants coming by flight can take bus from airport to Tambaram/Guindy. From there they can use Route 1 mentioned for participants reaching Tambaram or Route 3 mentioned for participants reaching Koyambedu. The buses from Koyembedu comes via Guindy. Further, they can also use cab facilities such as Ola, Fast track, etc.
+
+
 </p>
 </div>
 
